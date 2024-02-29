ef0e811bd6d69805732a69e7b7c96576e7cd10aa
Mikhail <kilai.ru@yandex.ru>
2024-03-01 01:31:31 +0900

    hw09-maven - CRUD приложение с Web UI и хранением данных в БД

--- hw09-maven/src/main/java/ru/otus/dto/requests/BooksRequest.java
+++ hw09-maven/src/main/java/ru/otus/dto/requests/BooksRequest.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.util.List;
+import java.util.Objects;
 
 @Getter
 @Setter
@@ -23,4 +24,15 @@ public final class BooksRequest {
                 "genres=" + genres + ']';
     }
 
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+
+        BooksRequest request = (BooksRequest) object;
+
+        if (!Objects.equals(name, request.name)) return false;
+        if (!Objects.equals(authors, request.authors)) return false;
+        return Objects.equals(genres, request.genres);
+    }
 }
---
+++ hw09-maven/src/test/java/ru/otus/controllers/CommentsControllerTest.java
@@ -0,0 +1,52 @@
+package ru.otus.controllers;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import ru.otus.services.CommentsService;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.forwardedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
+
+@WebMvcTest(CommentsController.class)
+class CommentsControllerTest {
+
+    private final static String TEST_COMMENT = "TEST_COMMENT";
+    private final static Long TEST_BOOK_ID = 1L;
+
+    @MockBean
+    private CommentsService service;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @DisplayName("Добавление комментария")
+    void create() throws Exception {
+        mockMvc.perform(
+                post("/comment")
+                        .contentType(MediaType.MULTIPART_FORM_DATA_VALUE)
+                        .param("bookId", TEST_BOOK_ID.toString())
+                        .param("comment", TEST_COMMENT))
+                .andExpect(redirectedUrl(String.format("/book/%d/comment", TEST_BOOK_ID)));
+
+        Mockito.verify(service, Mockito.times(1)).create(Mockito.any());
+    }
+
+    @Test
+    @DisplayName("Удаление комментария")
+    void delete() throws Exception {
+        mockMvc.perform(
+                MockMvcRequestBuilders.delete("/comment/" + TEST_BOOK_ID))
+                .andExpect(forwardedUrl("/"));
+
+        Mockito.verify(service, Mockito.times(1)).delete(TEST_BOOK_ID);
+    }
+}
